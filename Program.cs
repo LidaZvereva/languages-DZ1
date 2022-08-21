ccfcc34ae15a9c20b5ce07fea3c1a72d8793b58d
Lidia <lidia.wer@mail.ru>
2022-08-21 16:41:25 +0300

    работает вторая задача

--- Program.cs
+++ Program.cs
@@ -4,16 +4,43 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-System.Console.Write("Enter first number : ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Enter last number : ");
-int number2 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Enter first number : ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Enter last number : ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
 
-if(number1 < number2)
-{
-Console.WriteLine(number2);
-}
-else
-{
-Console.WriteLine(number1);
-}
+// if(number1 < number2)
+// {
+// Console.WriteLine(number2);
+// }
+// else
+// {
+// Console.WriteLine(number1);
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// System.Console.Write("Enter 1 number : ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Enter 2 number : ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Enter 3 number : ");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+
+// if ((number1 < number2) && (number2 < number3))
+// {
+// Console.WriteLine(number3);
+// }
+// else if ((number1 < number2) && (number2 > number3))
+// {
+// Console.WriteLine(number2);
+// }
+// else if ((number1 > number2) && (number1 > number3))
+// {
+// Console.WriteLine(number1);
+// }
