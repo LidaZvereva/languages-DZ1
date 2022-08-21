da242a9f9a4cb2a3c42dd2b5e7362a5a2f10532e
Lidia <lidia.wer@mail.ru>
2022-08-21 17:00:12 +0300

    работает третья задача

--- Program.cs
+++ Program.cs
@@ -44,3 +44,20 @@
 // {
 // Console.WriteLine(number1);
 // }
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+System.Console.Write("Enter number : ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+ if(number1 % 2 == 0)
+{
+Console.WriteLine( "Yes");
+}
+else
+{
+Console.WriteLine( "No");
+}
